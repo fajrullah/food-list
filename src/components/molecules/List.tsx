1840b1a049db0e40c493c73fe7fc9dabd96dde79
fajrullah sejati <fajrullahsejati@gmail.com>
2023-12-05 17:18:11 +0700

    feat: add list in molecules

---
+++ src/components/molecules/List.tsx
@@ -0,0 +1,36 @@
+import Label from "@/components/atoms/Label";
+import style from "./style.module.css";
+
+interface LabelProps {
+    readonly items: string[]
+}
+
+/**
+ * List Component
+ *
+ * A React component that renders an unordered list (ul) with items from an array.
+ *
+ * @component
+ *
+ * @param {object} props - The properties of the List component.
+ * @param {string[]} props.items - An array of strings representing the items to be displayed in the list.
+ *
+ * @returns {JSX.Element} List component.
+ *
+ * @example
+ * // Basic usage of the List component
+ * const myListItems = ["Item 1", "Item 2", "Item 3"];
+ * <List items={myListItems} />
+ */
+
+function List({ items }: LabelProps) {
+    return (
+        <ul className={style.horizontalList}>
+            {
+                items.map((key) => <li key={key}><Label label={key} /></li>)
+            }
+        </ul>
+    )
+}
+
+export default List;
