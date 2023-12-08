28726d671ce5be92fbf891c916ff728f2c49c2b9
fajrullah sejati <fajrullahsejati@gmail.com>
2023-12-08 16:37:50 +0700

    feat: unit testing for molecules

--- __tests__/components/molecules/List.test.tsx
+++ __tests__/components/molecules/List.test.tsx
@@ -1,13 +1,15 @@
 /**
  * @jest-environment jsdom
  */
+
 import List from '@/components/molecules/List';
-import { render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 
 jest.mock('@/components/atoms/Label', () => ({
   __esModule: true,
   default: jest.fn(({ label }) => <div data-testid="mock-label">{label}</div>),
 }));
+const onClick = jest.fn();
 
 describe('List component', () => {
   it('it should render a List', () => {
@@ -33,7 +35,7 @@ describe('List component', () => {
         name: 'Meat Ball',
       },
     ];
-    render(<List items={mock} />);
+    render(<List items={mock} onClick={() => {}} value="" />);
     const list = screen.getByRole('list');
     const { getAllByRole } = within(list);
     const items = getAllByRole('listitem');
@@ -55,7 +57,7 @@ describe('List component', () => {
         name: 'Burgers',
       },
     ];
-    render(<List items={mock} />);
+    render(<List items={mock} onClick={() => {}} value="" />);
     const list = screen.getByRole('list');
     const { getAllByRole, getAllByTestId } = within(list);
     const items = getAllByRole('listitem');
@@ -64,5 +66,48 @@ describe('List component', () => {
     items.forEach((item, index) => {
       expect(mockLabels[index]).toHaveTextContent(item.textContent);
     });
+  });
+
+  it('calls onClick callback when input value changes', async () => {
+    const mock = [
+      {
+        id: '1',
+        name: 'All',
+      },
+      {
+        id: '2',
+        name: 'Sushi',
+      },
+      {
+        id: '3',
+        name: 'Burgers',
+      },
+    ];
+    render(<List items={mock} onClick={onClick} value="" />);
+    const list = screen.getByRole('list');
+    const { getAllByRole } = within(list);
+    const items = getAllByRole('listitem');
+    items.forEach((item, index) => {
+      fireEvent.click(item);
+      expect(onClick).toHaveBeenCalled();
+    });
+  });
+
+  it('it should render a bacgkround an style', async () => {
+    const mock = [
+      {
+        id: '1',
+        name: 'All',
+      },
+      {
+        id: '2',
+        name: 'Sushi',
+      },
+    ];
+    render(<List items={mock} onClick={onClick} value="1" />);
+    const list = screen.getByRole('list');
+    const { getAllByRole } = within(list);
+    const items = getAllByRole('listitem');
+    expect(items[0]).toHaveClass('active');
   });
 });
