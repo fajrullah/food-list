3dc3d4cc03bd26c17779a8cd75b83ce1afe54b0a
fajrullah sejati <fajrullahsejati@gmail.com>
2023-12-08 16:45:35 +0700

    feat: organisms 100% coverage

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,18 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-// import Home from '@/pages/index'
-// import { render, screen } from '@testing-library/react'
-
 describe('Home', () => {
   it('renders a heading', () => {
-    // render(<Home />)
-
-    // const heading = screen.getByRole('heading', {
-    //   name: /welcome to next\.js!/i,
-    // })
-
-    // expect(heading).toBeInTheDocument()
-    expect(true).toBeTruthy()
-  })
-})
+    expect(true).toBeTruthy();
+  });
+});
